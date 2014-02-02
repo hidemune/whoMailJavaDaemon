bbea78bbb8ec0ab2df898daaeda0a648eaf355ac
Hidemune TANAKA <auau@kne.biglobe.ne.jp>
2014-02-02 22:13:13 +0900

    Update whoMailMain.sh

--- whoMailMain.sh
+++ whoMailMain.sh
@@ -9,7 +9,7 @@ do
   #バッククオートの結果に改行を残すために、ダブルクオートで囲む
   wk="`/usr/bin/who`"
   if [ "${wk}" != "${bk}" ] ; then
-    echo "`/usr/bin/who`" | /usr/bin/java -cp /home/hdm/tool/MyJavamail/lib/*.jar -jar /home/hdm/tool/MyJavamail/javamail.jar "Console Access" "tanaka-hidemune@ezweb.ne.jp"
+    echo "`/usr/bin/who`" | /usr/bin/java -cp /home/xxxx/lib/*.jar -jar /home/xxxx/javamail.jar "Console Access" "xxxx@xxxx.jp"
   fi
   bk=${wk}
   sleep 1
