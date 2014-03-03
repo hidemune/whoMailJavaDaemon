6c850a63d96f563315e877241e3f6e7f2be5df39
Hidemune TANAKA <auau@kne.biglobe.ne.jp>
2014-03-03 17:26:05 +0900

    Update whoMailMain.sh

--- whoMailMain.sh
+++ whoMailMain.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-echo "起動成功" >> /home/xxxx/who.log
-
 #cd `/usr/bin/dirname $0` これをやると他のPGでエラー発生
 
 while (true)
@@ -9,7 +7,7 @@ do
   #バッククオートの結果に改行を残すために、ダブルクオートで囲む
   wk="`/usr/bin/who`"
   if [ "${wk}" != "${bk}" ] ; then
-    echo "`/usr/bin/who`" | /usr/bin/java -cp /home/xxxx/lib/*.jar -jar /home/xxxx/javamail.jar "Console Access" "xxxx@xxxx.jp"
+    echo "`/usr/bin/w`" | /usr/bin/java -cp /home/xxxx/lib/*.jar -jar /home/xxxx/javamail.jar "Console Access" "xxxx@xxxx.jp"
   fi
   bk=${wk}
   sleep 1
